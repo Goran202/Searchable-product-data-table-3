69dc4ddcc4f1f0273837a56fac2ef9d81c9f4430
Goran Ljaljić <gljaljic@gmail.com>
2019-09-16 18:40:03 +0200

    Minor

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,9 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    jest: true,
   },
-  parser: 'babel-eslint',
-  extends: ['airbnb'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'airbnb'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -16,9 +16,28 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  parser: 'babel-eslint',
   plugins: ['react'],
   rules: {
+    'linebreak-style': ['error', 'windows'],
+    //'comma-dangle': ['error', 'never'],
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/state-in-constructor': ['error', 'never'],
+    'react/forbid-prop-types': 0,
+    camelcase: 0,
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+      },
+    ],
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectPattern: { multiline: true },
+      },
+    ],
+    'implicit-arrow-linebreak': 0,
+    'react/jsx-props-no-spreading': 0,
+    //"no-undef": 0
   },
 };
