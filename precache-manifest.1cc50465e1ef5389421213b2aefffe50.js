2be89cbdacb27994d3cf62fdbe7fc6204ab1483b
Goran Ljaljić <gljaljic@gmail.com>
2019-09-17 08:50:25 +0200

    Updates

---
+++ precache-manifest.1cc50465e1ef5389421213b2aefffe50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12562d58853c5d7acf53ccc0bdf2651e",
+    "url": "/Goran202/Searchable-product-data-table-3.git/index.html"
+  },
+  {
+    "revision": "3899dce27b478d232543",
+    "url": "/Goran202/Searchable-product-data-table-3.git/static/css/main.1536c332.chunk.css"
+  },
+  {
+    "revision": "c9a7cbc3bc85e7a10de2",
+    "url": "/Goran202/Searchable-product-data-table-3.git/static/js/2.da5d1ec3.chunk.js"
+  },
+  {
+    "revision": "3899dce27b478d232543",
+    "url": "/Goran202/Searchable-product-data-table-3.git/static/js/main.29630388.chunk.js"
+  },
+  {
+    "revision": "f5b796543e33a510d937",
+    "url": "/Goran202/Searchable-product-data-table-3.git/static/js/runtime~main.7a01f5d6.js"
+  }
+]);
